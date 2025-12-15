863865facfcb53bf1a43921f70dd9cb01e091adb
Jun Park <junpark1041@gmail.com>
2025-12-15 10:32:40 -0500

    fixed and simplified main.rs

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,6 @@
 use std::error::Error;
-use sqlx::Row;
+use sqlx::{Row, PgPool};
 use sqlx::postgres;
-
-use std::collections::HashSet;
-use rand::Rng;
 
 use axum::{routing::{get, post},  Router};
 
@@ -18,40 +15,6 @@ type SimpleResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 struct ConnectionState {
     pub db: PgPool,
 }
-
-
-///may need to change this to allow DB to generate user IDs outside of Rust 
-impl User {
-    pub fn new(username: String) -> SimpleResult<Self> {
-        let mut rng = rand::rng();
-        let mut userid: i32 = rng.random();
-
-        //If the randomly selected ID is already taken, rerandomize
-        while activeusers.contains(userid) {
-            userid = rng.random();
-        }
-
-        //Add to list of users present
-        activeusers.insert(userid);
-
-        //Return self
-        Ok(Self {
-            unique_id: userid;
-            name: username;
-        })
-    }
-
-    //Return self name
-    pub fn get_name() -> SimpleResult<String> {
-        Ok(Self.name)
-    }
-
-    //Return self ID
-    pub fn get_id() -> SimpleResult<i32> {
-        Ok(Self.unique_id)
-    }
-}
-
 
 /// taken from https://github.com/launchbadge/sqlx
 /// sqlx is used to manager our database 
@@ -72,35 +35,13 @@ async fn main() -> Result<(), sqlx::Error> {
     ///creating router for Axum for chatroom HTTP methods; https://docs.rs/axum/latest/axum/routing/struct.Router.html
     let app = Router::new().route("/health", get(health_check))
         /// may only need this route for post and get 
-        .route("/messages", post(message).get(message))
-        .route("/users", get(list_users))
-        .route("/users/{id}", get(get_user))
-        // .route("/messages", get(move |mid| message_history(mid)))
+        .route("/messages", post(create_message).get(get_all_messages))
         .with_state(pool.clone());
 
+    //to run the server
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    println!("Server running on http://0.0.0.0:3000");
+    axum::serve(listener, app).await?;
+
     Ok(())
-}
-
-
-
-#[cfg(test)]
-mod test {
-
-    #[test]
-    fn create_user() {
-        let username = "UserA".to_string();
-        let user = username::new(username);
-        assert_eq!(
-            user.get_name(), "UserA".to_string()
-        );
-    }
-
-    #[test]
-    fn hashset_push() {
-        let username = "UserA".to_string();
-        let user = username::new(username);
-        assert!(
-            activeusers.contains(user)
-        );
-    }
 }
